90412427db231dbfe20f688eac36c729a25b8a2d
david horovitz <dovidho@gmail.com>
2025-05-21 17:06:37 +0300

    first_init

---
+++ ArtilleryM109.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFproject_2105
+{
+    internal class ArtilleryM109: IWapons, IAmmunition, IEffectiveAgainst, IEnergyBalance , ITypeOfBomb
+    {
+        string Name = "shell";
+        public string NameOfWapon(string name)
+        {
+
+            return name;
+        }
+
+        public string NameOfAmmunition(string name)
+        {
+
+            return name;
+        }
+
+        public string NameOfTargt(string[] names)
+        {
+
+            return names[0];
+        }
+        public int LeftEnergyBalance(int num)
+        {
+
+            return num;
+        }
+        public string GetTypeOfBomb(string name)
+        {
+            return name;
+        }
+    }
+}
---
+++ F16.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFproject_2105
+{
+    internal class F16 : IWapons, IAmmunition, IEffectiveAgainst, IEnergyBalance, ITypeOfBomb
+    {
+        string Name = "1 ton or 0.5 ton";
+
+        public string NameOfWapon(string name)
+        {
+            return name;    
+        }
+        public string NameOfAmmunition(string name)
+        {
+            return name;
+        }
+        public string NameOfTargt(string[] names)
+        {
+            return names[0];
+        }
+        public int LeftEnergyBalance(int num)
+        {
+            return num;
+        }
+        public string GetTypeOfBomb(string name)
+        {
+            return name;
+        }
+
+
+    }
+}
