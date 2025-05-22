e0ddd85c63f1ac7a2814c8f7ac93afdb5f24d100
baruch <baruchminzberg@gmail.com>
2025-05-22 17:00:51 +0300

    hamas end terorist

---
+++ Hamas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IDF_Project
+{
+    internal class Hamas
+    {
+        Terrorist Terrorist;
+        string YearOfEstablishment = "1987";
+        string HeadOfOrganization = "Yahya Sanwar";
+
+        public Dictionary <string,string> terroristsDictionary = new Dictionary <string,string> ();
+
+        public Hamas(string a, string b)
+        {
+            a = YearOfEstablishment;
+            b = HeadOfOrganization;
+            Console.WriteLine($"Year Of Establishment: {YearOfEstablishment}\n "+
+            $"Head Of Organization; {HeadOfOrganization}");
+        }
+        public void AddTNewTeroris(string enterTerrorist)
+        {
+            //Terrorist a = new Terrorist(enterTerrorist);
+            //a = a.ToString;
+            terroristsDictionary.Add("name: ", enterTerrorist);
+        }
+    }
+}
---
+++ Terrorists.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IDF_Project
+{
+    internal class Terrorist
+    {
+        string NameOfTrrorist;       
+        public Terrorist(string nameOfTrrorist)
+        {
+            this.NameOfTrrorist = nameOfTrrorist;
+        }
+        public string EnterTerroristLevel()
+        {
+            Console.WriteLine("Please enter terrorist level");
+            string level = Console.ReadLine();
+            return level;
+        }
+        public string EnterTerroristStatus()
+        {
+            Console.WriteLine("Please enter terrorist status");
+            string status = Console.ReadLine();
+            return status;
+        }
+        public string EnterTerroristWeapons()
+        {
+            Console.WriteLine("Please enter terrorist weapons");
+            string weapons = Console.ReadLine();
+            return weapons;
+        }
+    }
+}
