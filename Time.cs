8e1f135a330043dcee74c8f596e84a01c50504d6
david horovitz <dovidho@gmail.com>
2025-05-27 15:57:30 +0300

    aman menu and time

---
+++ Time.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_Project
+{
+    internal class Time
+    {
+        public string GetTime()
+        {
+            DateTime currentTime = DateTime.Now;
+           return currentTime.ToString();
+            
+        }
+            
+    }
+}
